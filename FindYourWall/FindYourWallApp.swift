2eac074ac42637a348cf237444c4c60feaed445c
Max Wayne <maxwn5@gmail.com>
2025-11-15 13:49:52 -0700

    Initial commit

---
+++ FindYourWall/FindYourWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourWallApp.swift
+//  FindYourWall
+//
+//  Created by Max Wayne on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FindYourWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindYourWallTests/FindYourWallTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourWallTests.swift
+//  FindYourWallTests
+//
+//  Created by Max Wayne on 11/15/25.
+//
+
+import Testing
+@testable import FindYourWall
+
+struct FindYourWallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
