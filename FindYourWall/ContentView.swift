c61cd8a798172ef8fadf7026884d2318afcb8bdb
Max Wayne <maxwn5@gmail.com>
2025-11-15 19:14:07 -0700

    App now displays users location on launch

--- FindYourWall/ContentView.swift
+++ FindYourWall/ContentView.swift
@@ -6,19 +6,42 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+   
+    @State private var cameraPosition: MapCameraPosition = .region(
+        .init(center: .empowerStadium, span: Constants.defaultSpan)
+    )
+    
+    private struct Constants {
+        static let defaultSpan: MKCoordinateSpan = .init(latitudeDelta: 0.01,
+                                                         longitudeDelta: 0.01)
     }
+    
+    var body: some View {
+        Map(position: $cameraPosition) {
+            UserAnnotation()
+        }
+        .onAppear {
+            updateCameraPositionToUserLocation()
+        }
+    }
+    
+    private func updateCameraPositionToUserLocation() {
+        if let userLocation = CLLocationManager().location?.coordinate {
+            cameraPosition = .region(
+                .init(center: userLocation, span: Constants.defaultSpan)
+            )
+        }
+    }
+    
 }
 
 #Preview {
     ContentView()
+}
+
+extension CLLocationCoordinate2D {
+    static let empowerStadium = CLLocationCoordinate2D(latitude: 39.7439, longitude: 105.0201)
 }
