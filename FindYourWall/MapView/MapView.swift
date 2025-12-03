1b23a18861d77729190aa5bfc1df8694f0af01f2
Max Wayne <maxwn5@gmail.com>
2025-12-02 18:11:27 -0700

    Tapping a marker now displays a sheet, but the sheet UI still needs to be designed and I need to refactor the sheet UI into its own view with its own view model

--- FindYourWall/MapView/MapView.swift
+++ FindYourWall/MapView/MapView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     @State private var currentRegion: MKCoordinateRegion = .init(center: .empowerStadium, span: MapViewModel.Constants.defaultSpan)
     @State private var selectedTag: Int?
+    @State private var showMarkerSheet = false
     @State private var viewModel = MapViewModel()
     
     var body: some View {
@@ -30,6 +31,37 @@ struct ContentView: View {
             }
             .onChange(of: self.selectedTag) {
                 print("Tag tapped: \(String(describing: self.selectedTag))")
+                if self.selectedTag != nil {
+                    self.showMarkerSheet = true
+                }
+            }
+            .sheet(isPresented: self.$showMarkerSheet) {
+                // TODO: Design how I want this sheet UI to look
+                // TODO: Refactor this into its own view class that has its own view model
+                if let selectedTag = self.selectedTag {
+                    VStack(alignment: .leading, spacing: 16) {
+                        let mapItem = self.viewModel.mapSearchResults[selectedTag]
+                        Text(mapItem.name ?? "")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.top, 8)
+                        
+                        // TODO: Display the map item's address
+                        Text("12345 Generic Ave.")
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(12)
+                        
+                        // TODO: Eventually we will be able to save locations
+                        Button(action: {
+                            print("Sheet button tapped")
+                        }) {
+                            Text("Add")
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    .presentationDetents([.medium])
+                }
             }
             .safeAreaInset(edge: .bottom) {
                 self.searchBox
